92da03f4c503cb9c93e95930cfc5ceb92687abd5
indu898 <erik.ostlind@gmail.com>
2024-07-24 15:27:13 +0200

    done the first 15 lessons

--- tests/UIBasicstest.spec.js
+++ tests/UIBasicstest.spec.js
@@ -9,19 +9,67 @@ test('first playwright test', async ({browser})=>{ }) // curly braces to make su
 
 
 */
-test('First Playwright test', async ({browser}) =>
+test('First Playwright test - Invalid login', async ({browser}) =>
  {
     //Create chrome instance - plugins/cookies. Used when injecting cookies into context.
     const context =  await browser.newContext()
     const page = await context.newPage()
 
-    await page.goTo("https://rahulshettyacademy.com/loginpagePractise");
+    const username = page.locator('#username');
+    const password = page.locator('#password');
+    const signIn = page.locator('#signInBtn');
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise");
+
+    await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy');
+    await username.fill('rahulshetty');
+    await password.fill("secret");
+    await signIn.click();
+    
+    //Get the nth element text
+    console.log(await expect(page.locator("[style*='block']")).nth(0).toContainText('Incorrect username/password.'));
+
+    console.log(await expect(page.locator("[style*='block']")).nth(0).toContainText('Incorrect username/password.'));
+
+
+
  }
  )
- test('Second Playwright test', async ({page}) =>
+ test('Second Playwright test, Valid login, multiple objects', async ({page}) =>
  {
-    // No setup required just start with page context.
-    await page.goTo("https://rahulshettyacademy.com/loginpagePractise");
+    const username = page.locator('#username');
+    const password = page.locator('#password');
+    const signIn = page.locator('#signInBtn');
+
+    await page.goto("https://rahulshettyacademy.com/loginpagePractise");
+
+    await expect(page).toHaveTitle('LoginPage Practise | Rahul Shetty Academy');
+    await username.fill('rahulshettyacademy');
+    await password.fill("learning");
+    await signIn.click();
+    //Store all text in an array - Need to run with proper wait.
+    const allTitles = await page.locator(".card-body a").allTextContents();
+    console.log(allTitles)
+   
+    //Get the nth element text
+     console.log(await page.locator(".card-body a").nth(1).textContent());
+   
+     //Get the first element text
+     console.log(await page.locator(".card-body a").first().textContent());
+
+
+    
+    
+   
+
     
  }
+
+ 
+ /*
+ //rahulshettyacademy.com/client/auth/login
+ So register here,
+ log in to page
+ Find the first project item
+ */
  )
